40b5b17da4721d49c8779856b15aa27f0ba2e3f9
vegetau <uppoorabhiram@outlook.com>
2017-09-18 16:04:41 +0530

    UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,40 @@
 var socket = io();
 socket.on('connect', function () {
-  console.log('Connected to server');
+    console.log('Connected to server');
 });
 
 socket.on('disconnect',function () {
-  console.log('Disconnected from server');
+    console.log('Disconnected from server');
 });
 
 socket.on('newMessage',function (obj) {
-  console.log('Message received',obj);
+    console.log('Message received',obj);
+    
+    var li =document.createElement("li");
+    var text = document.createTextNode(obj.from+":"+obj.text);
+    li.appendChild(text);
+    var ul = document.getElementById("messages").appendChild(li);
 });
 
-socket.emit('createMessage',{
-  from:'ww@wb.com',
-  text:'K'
-});
+/*socket.emit('createMessage',{
+    from:'ww@wb.com',
+    text:'K'
+}, function (data) {
+    console.log('Acknowledged',data);
+});*/
+
+
+var form = document.getElementById('message-form');
+
+form.addEventListener("submit", function(e){
+    
+    e.preventDefault();
+    
+    socket.emit('createMessage',{
+        from:'User',
+        text:document.getElementsByName("message")[0].value
+    } , function() {
+      console.log('Acknowledge');  
+    });
+    
+},false);
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
+const generateMessage = require('./util/message').generateMessage;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -17,27 +19,15 @@ io.on('connection', function(socket) {
     console.log(' inside disConnect');
   });
 
-  socket.emit('newMessage', {
-    from:'Admin',
-    text:'Welcome to the chat',
-    createdAt:new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('Admin','Welcome to chat application'));
 
-  socket.broadcast.emit('newMessage', {
-    from:'Admin',
-    text:'New usser joined',
-    createdAt:new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-  socket.on('createMessage',function (message) {
+  socket.on('createMessage',function (message,callback) {
     console.log('Message received',message);
 
-    // io.emit('newMessage',{
-    //   from:message.from,
-    //   text:message.text,
-    //   createdAt:new Date().getTime()
-    // });
-
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('This is from server');
     // socket.broadcast.emit('newMessage', {
     //   from:message.from,
     //   text:message.text,
